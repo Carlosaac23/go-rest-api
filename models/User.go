cda42d012ff2908200cd7bd3d38541ca9cfb839b
Carlos <carlosaac232001@gmail.com>
2026-03-21 08:58:18 -0500

    chore: update User type with id field

--- models/User.go
+++ models/User.go
@@ -5,6 +5,7 @@ import "gorm.io/gorm"
 type User struct {
 	gorm.Model
 
+	ID        string
 	FirstName string `gorm:"not null"`
 	LastName  string `gorm:"not null"`
 	Email     string `gorm:"not null;unique"`
