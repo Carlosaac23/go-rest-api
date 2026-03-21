a4b892306385d3ee6621cc2fabf31bd71d3b7767
Carlos <carlosaac232001@gmail.com>
2026-03-21 10:21:02 -0500

    chore: add json fields to every User type field

--- models/User.go
+++ models/User.go
@@ -6,9 +6,9 @@ type User struct {
 	gorm.Model
 
 	ID        string
-	FirstName string `gorm:"not null"`
-	LastName  string `gorm:"not null"`
-	Email     string `gorm:"not null;unique"`
-	Password  string `gorm:"not null"`
-	Tasks     []Task
+	FirstName string `gorm:"not null" json:"first_name"`
+	LastName  string `gorm:"not null" json:"last_name"`
+	Email     string `gorm:"not null;unique" json:"email"`
+	Password  string `gorm:"not null" json:"password"`
+	Tasks     []Task `json:"tasks"`
 }
